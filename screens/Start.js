76d643e2b7bf18507b39375f2924455f0a6186dd
AmeenPopoola <20231717@studentmail.ul.ie>
2024-02-02 15:31:57 +0000

    added recommended font style+seperated images into assets/images

--- screens/Start.js
+++ screens/Start.js
@@ -1,17 +1,27 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { View, Text, Image, StyleSheet,TouchableOpacity } from 'react-native';
+import { useFonts, PTSerif_400Regular, PTSerif_700Bold } from '@expo-google-fonts/pt-serif';
 
 const Start = () => {
-    const navigation = useNavigation();
 
-    const handleStartPress =() => {
-        navigation.navigate('Setup');
-    };
+  const navigation = useNavigation();
+
+  const handleStartPress =() => {
+    navigation.navigate('Setup');
+  };
+
+  let [fontsLoaded] = useFonts({
+    PTSerif_400Regular,PTSerif_700Bold
+  });
+  
+  if (!fontsLoaded) {
+    return null;
+  }
 
   return (
     <View style={styles.container}>
-      <Image source={require('../assets/logo.png')} style={styles.logo} />
+      <Image source={require('../assets/images/logo.png')} style={styles.logo} />
       <Text style={styles.headerText}>HeartMate</Text>
       <Text style={styles.welcomeText}>
         Welcome to HeartMate,an application for managing your heart health!
@@ -31,7 +41,7 @@ const styles = StyleSheet.create({
       backgroundColor: '#FFFFFF', 
     },
     headerText: {
-        fontFamily: 'Helvetica',
+        fontFamily: 'PTSerif_700Bold',
         fontSize: 24,
         fontWeight:'bold',
         color: '#000000',
@@ -45,20 +55,20 @@ const styles = StyleSheet.create({
     welcomeText: {
       textAlign: 'center',
       fontSize: 20,
-      fontFamily: 'Helvetica',
+      fontFamily: 'PTSerif_400Regular',
       color: '#000000', 
     },
     startButton: {
-        backgroundColor: '#000000', 
-        padding: 20,
-        borderRadius: 5,
-        width:'95%',
-        position: 'absolute',
-        bottom:'15%',
+      backgroundColor: '#000000', 
+      padding: 20,
+      borderRadius: 5,
+      width:'95%',
+      position: 'absolute',
+      bottom:'15%',
       },
       startButtonText: {
         fontSize: 18,
-        fontFamily: 'Helvetica',
+        fontFamily: 'PTSerif_400Regular',
         color: '#FFFFFF', 
         textAlign: 'center',
       },
